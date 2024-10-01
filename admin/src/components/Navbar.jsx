1810e363d92a824b3cfabd6b8b051c861482f304
Akansha Salwan <akankshasalwan@gmail.com>
2024-10-01 13:58:09 +0530

    admin section update

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -20,9 +20,9 @@ const Navbar = () => {
   }
 
   return (
-    <div className='flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white'>
+    <div className='flex justify-between items-center px-3 h-20 sm:px-10 py-2border-b bg-white'>
       <div className='flex items-center gap-2 text-xs'>
-        <img onClick={() => navigate('/')} className='w-36 sm:w-40 cursor-pointer' src={assets.care_now} alt="" />
+        <img onClick={() => navigate('/')} className='w-40 h-55  cursor-pointer' src={assets.care_now} alt="" />
         <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>{aToken ? 'Admin' : 'Doctor'}</p>
       </div>
       <button onClick={() => logout()} className=' text-white text-sm px-10 py-2 rounded-full'  style={{ backgroundColor: '#3ED2D1' }}>Logout</button>
